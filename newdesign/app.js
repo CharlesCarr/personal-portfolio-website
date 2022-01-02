9c412445fdefffb7160862fec9bed1c8669ee81d
Charlie Carr <57736161+CharlesCarr@users.noreply.github.com>
2022-01-01 23:46:00 -0500

    smooth scroll vanilla js for nav

--- newdesign/app.js
+++ newdesign/app.js
@@ -17,3 +17,64 @@ function hamburgerFunc() {
 };
 
 // Need to write a function for when a selection is made to close the menu
+
+
+
+// SMOOTH SCROLL JS
+
+function smoothScroll(target, duration) {
+// const smoothScroll = (target, duration) => {
+    let targetTarget = document.querySelector(target);
+    // How far from top of the screen
+    let targetPosition = targetTarget.getBoundingClientRect().top;
+    // Relative to the window not the actual element
+    let startPosition = window.pageYOffset;
+    // Distance between the two points
+    let distance = targetPosition - startPosition;
+    let startTime = null;
+
+    function animation(currentTime) {
+    // const animation = (currentTime) => {
+        if (startTime === null ) startTime = currentTime;
+        let timeElapsed = currentTime - startTime;
+
+        let run = ease(timeElapsed, startPosition, distance, duration);
+            window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+        
+    }
+
+    function ease (t, b, c, d) {
+    // const ease = (t, b, c, d) => {
+        t /= d/2;
+	    if (t < 1) return c/2*t*t*t + b;
+	    t -= 2;
+	    return c/2*(t*t*t + 2) + b;
+    }
+
+
+
+    requestAnimationFrame(animation);
+
+}
+
+const navAbout = document.getElementById('about-nav');
+const navProjects = document.getElementById('projects-nav');
+const navContact = document.getElementById('contact-nav');
+
+
+navAbout.addEventListener('click', function() {
+    console.log('working');
+    smoothScroll('#about-section', 2000);
+});
+
+navProjects.addEventListener('click', function() {
+    console.log('working');
+    smoothScroll('#projects-container', 2000);
+});
+
+
+navContact.addEventListener('click', function() {
+    console.log('working');
+    smoothScroll('#contact-section', 2000);
+});
